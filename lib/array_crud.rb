7e28423662c41463dc98933b48c2b013b7c36a9f
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-21 02:02:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,35 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  number = [77, 88, 99, 66]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  number = [77, 88, 99, 66]
+  number << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  number = [77, 88, 99, 66]
+  number.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
